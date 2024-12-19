5a15c892940bcb9d1df126b688308face6ea7f2d
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-12-19 17:40:13 +0530

    config update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ["media.crafto.app"],
+    unoptimized: true,
+  },
+  output: "export",
+  typescript: {
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
