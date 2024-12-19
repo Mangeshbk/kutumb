15cf459f099a165bff64500e6bb786e712777528
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-12-19 17:44:59 +0530

    main route update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import QuoteList from "@/components/quoteList";
 import "./page.css";
+import PageWrapper from "@/components/wrapper/pageWrapper";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <PageWrapper>
+        <QuoteList />
+      </PageWrapper>
+    </main>
+  );
 }
