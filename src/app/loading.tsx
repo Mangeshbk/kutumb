763f631984d1cb4caef30523e534ec439b7c44b6
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-12-19 17:40:57 +0530

    global laoding page added

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import PageLoader from "@/components/pageLoader";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div>
+      <PageLoader />
+    </div>
+  );
+}
