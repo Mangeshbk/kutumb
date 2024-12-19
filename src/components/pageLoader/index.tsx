d7fa9aa5d438f935c853938d11f164d6ad22a1e4
Mangesh Landge <mangeshbkwsu@gmail.com>
2024-12-19 17:44:35 +0530

    create global page load component

---
+++ src/components/pageLoader/index.tsx
@@ -0,0 +1,14 @@
+import "./index.css";
+export default function PageLoader() {
+  return (
+    <div className='loader_container'>
+      <div className='loader3'>
+        <div className='circle1'></div>
+        <div className='circle1'></div>
+        <div className='circle1'></div>
+        <div className='circle1'></div>
+        <div className='circle1'></div>
+      </div>
+    </div>
+  );
+}
